bb53b22e15d899176b839748b80bc63c2fa777b2
Strus Sviatoslav <svyatoslav@email.ua>
2016-10-18 15:18:48 +0300

    1

--- src/com/javarush/test/level18/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus01/Solution.java
@@ -2,7 +2,6 @@ package com.javarush.test.level18.lesson10.bonus01;
 
 /* Шифровка
 Придумать механизм шифровки/дешифровки
-
 Программа запускается с одним из следующих наборов параметров:
 -e fileName fileOutputName
 -d fileName fileOutputName
@@ -13,9 +12,30 @@ fileOutputName - имя файла, куда необходимо записат
 -d - ключ указывает, что необходимо расшифровать данные
 */
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Solution {
     public static void main(String[] args) {
-
+        String fileName = args[1];
+        String fileOutputName = args[2];
+        try {
+            crypt(fileName, fileOutputName);
+        } catch (Exception e) {
+            System.out.println("Error!");
+            e.printStackTrace();
+        }
     }
 
+    public static void crypt(String f1, String f2) throws IOException {
+        FileInputStream in = new FileInputStream(f1);
+        FileOutputStream out = new FileOutputStream(f2);
+        while (in.available() > 0) {
+            int data = in.read();
+            out.write(data^5);
+        }
+        in.close();
+        out.close();
+    }
 }
