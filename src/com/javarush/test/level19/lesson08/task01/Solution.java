cb4265d9850f802373d701c8cea0cb295d11e701
Strus Sviatoslav <svyatoslav@email.ua>
2017-01-15 23:23:25 +0200

    1

--- src/com/javarush/test/level19/lesson08/task01/Solution.java
+++ src/com/javarush/test/level19/lesson08/task01/Solution.java
@@ -8,10 +8,30 @@ package com.javarush.test.level19.lesson08.task01;
 Вывести модифицированную строку в консоль.
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        String newText = outputStream.toString().toUpperCase();
+
+        System.setOut(consoleStream);
+
+        System.out.println(newText);
     }
 
     public static class TestString {
